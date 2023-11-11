ef7d7b15de1645aad0a56de25e0d63b23a7c6a40
jakubzaliwski <jakub.zaliwski@student.put.poznan.pl>
2023-11-11 23:20:16 +0100

    Create zadanie 3.py

---
+++ zadanie 3.py
@@ -0,0 +1,39 @@
+# Napisz funkcję, która wczyta do listy liczby z pliku z poprzedniego zadania. Napisz funkcje do wyznaczenia
+# średniej, odchylenia standardowego, wartości maksymalnej oraz minimalnej wczytanych liczb a następnie
+# wywołaj je i otrzymane wartości wyświetl na ekranie. Następnie posortuj wczytane liczby malejąco i
+# ponownie wyświetl je na ekranie. Nie korzystaj z z biblioteki numpy.
+import random
+import statistics
+
+def losowe_liczby(nazwa_pliku, n):
+    lista = []
+    with open(nazwa_pliku, 'w') as plik:
+        for _ in range(n):
+            liczba = random.randint(1, 100)
+            plik.write(str(liczba) + '\n')
+            lista.append(liczba)
+        return lista
+
+
+
+try:
+    n = int(input("Podaj ilość elementow: "))
+    lista = losowe_liczby('zad3.txt', n)
+
+    srednia = round(statistics.mean(lista))
+    odchylenie_standardowe = round(statistics.stdev(lista))
+    lista_posortowana = sorted(lista, reverse=True)
+    lista_zrewersowana = sorted(lista, reverse=False)
+    with open('zad3.txt', 'w') as plik:
+        print("Pomyslnie zapisano " + str(n) + " elementow", file=plik)
+        print("lista liczb: ", lista, file=plik)
+        print("Maksymalna wartosc to: ", max(lista), file=plik)
+        print("minimalna wartosc to: ", min(lista), file=plik)
+        print("srednia calej listy wynosi: ", srednia, file=plik)
+        print("odchylenie standardowe wynosi: ", odchylenie_standardowe, file=plik )
+        print("Lista wczytana malejaco: ", lista_posortowana, file=plik)
+        print("Lista wczytana rosnaco: ", lista_zrewersowana, file=plik)
+
+except ValueError:
+    print("błąd, nieprawidłowe dane podane")
+
