8920a3ddbd15be946dfa94922f22f287b71f7012
jakubzaliwski <150194086+jakubzaliwski@users.noreply.github.com>
2023-11-22 13:13:42 +0100

    Update zadanie 3.py

--- zadanie 3.py
+++ zadanie 3.py
@@ -1,9 +1,8 @@
 # Napisz funkcję, która wczyta do listy liczby z pliku z poprzedniego zadania. Napisz funkcje do wyznaczenia
 # średniej, odchylenia standardowego, wartości maksymalnej oraz minimalnej wczytanych liczb a następnie
 # wywołaj je i otrzymane wartości wyświetl na ekranie. Następnie posortuj wczytane liczby malejąco i
-# ponownie wyświetl je na ekranie. Nie korzystaj z z biblioteki numpy.
+# ponownie wyświetl je na ekranie. Nie korzystaj z z biblioteki nu
 import random
-import statistics
 
 def losowe_liczby(nazwa_pliku, n):
     lista = []
@@ -14,14 +13,24 @@ def losowe_liczby(nazwa_pliku, n):
             lista.append(liczba)
         return lista
 
+def odchylenie_stand(l):
+    i = 0
+    sum_wariancja = 0
 
+    while i < len(l):
+        sum_wariancja += (l[i] - srednia) ** 2
+        i += 1
+
+    wariancja = sum_wariancja / len(lista)
+    odchylenie_standardowe = (wariancja ** 0.5)
+    return odchylenie_standardowe
 
 try:
     n = int(input("Podaj ilość elementow: "))
     lista = losowe_liczby('zad3.txt', n)
+    srednia = sum(lista) / len(lista)
+    odchylenie = odchylenie_stand(lista)
 
-    srednia = round(statistics.mean(lista))
-    odchylenie_standardowe = round(statistics.stdev(lista))
     lista_posortowana = sorted(lista, reverse=True)
     lista_zrewersowana = sorted(lista, reverse=False)
     with open('zad3.txt', 'w') as plik:
@@ -30,10 +39,9 @@ try:
         print("Maksymalna wartosc to: ", max(lista), file=plik)
         print("minimalna wartosc to: ", min(lista), file=plik)
         print("srednia calej listy wynosi: ", srednia, file=plik)
-        print("odchylenie standardowe wynosi: ", odchylenie_standardowe, file=plik )
+        print("odchylenie standardowe wynosi: ", odchylenie, file=plik )
         print("Lista wczytana malejaco: ", lista_posortowana, file=plik)
         print("Lista wczytana rosnaco: ", lista_zrewersowana, file=plik)
 
 except ValueError:
     print("błąd, nieprawidłowe dane podane")
-
