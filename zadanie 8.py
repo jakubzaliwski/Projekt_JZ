aa23ee168545188198aeb631e415d7a2937985fd
jakubzaliwski <150194086+jakubzaliwski@users.noreply.github.com>
2023-12-10 02:19:58 +0100

    Nie mam pojecia czy dobrze

--- zadanie 8.py
+++ zadanie 8.py
@@ -1,2 +1,16 @@
+from datetime import datetime
 import random
-improt datetime from datetime
+
+def create_files():
+    for _ in range(1, 11):
+        current_time = datetime.now()
+        current_time_str = current_time.strftime("%Y_%m_%d_%H_%M_%S_%f")
+        file_name = f"{current_time_str}.bin"
+
+        with open(file_name, 'wb') as file:
+            for _ in range(10):
+                number = random.randint(1, 100)
+                file.write(str(number).encode() + b'\n')
+
+create_files()
+
